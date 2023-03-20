f3e2082cdda2970e7aacd789f9dda2a3365c3f7c
Kim, Dong H <ehdgus1524@gmail.com>
2023-03-20 11:41:12 +0900

    wip

--- libraries/linear/linearPlanning.lib.go
+++ libraries/linear/linearPlanning.lib.go
@@ -3,14 +3,27 @@ package linear
 import "fmt"
 
 var (
-	x1Name string
-	x2Name string
-	x1     float64
-	x2     float64
+	x1Name   string
+	x2Name   string
+	isMax    int
+	maxAnser string
+	x1       float64
+	x2       float64
 )
 
 func LinearPlanning() {
 	fmt.Println("선형 계획법")
+
+	fmt.Println("최대화 문제인가요? y / n")
+	fmt.Scanln(&maxAnser)
+	if maxAnser == "y" {
+		isMax = 1
+	} else if maxAnser == "n" {
+		isMax = 0
+	} else {
+		fmt.Println("유효하지 않은 입력값")
+		return
+	}
 
 	fmt.Println("첫번째 의사결정 변수 이름을 알려주세요")
 	fmt.Println("예시: 제품 갑의 생산량")
